c18f6c968771de84020bb14654d8af21bb988a96
Vin <phamvanvinh8a2@gmail.com>
2024-01-20 03:32:55 +0700

    tag, series

---
+++ src/main/java/org/caykhe/contentservice/dtos/SeriesDto.java
@@ -0,0 +1,36 @@
+package org.caykhe.contentservice.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.Instant;
+import java.util.List;
+
+@Data
+@Builder
+public class SeriesDto {
+    Integer id;
+
+    @NotBlank(message = "Tiêu đề không được để trống")
+    @Size(max = 100, message = "Tiêu đề không được quá 100 ký tự")
+    String title;
+
+    @NotBlank(message = "Nội dung không được để trống")
+    String content;
+
+    List<Integer> postIds;
+
+    @NotNull(message = "Mức độ chia sẻ phải được chỉ định")
+    Boolean isPrivate;
+
+    Integer score;
+
+    Integer commentCount;
+
+    Instant updatedAt;
+
+    String createdBy;
+}
